2a007870565c941c05ff473e67efa9a8b3800586
Iman Kiani <kianiman@gmail.com>
2018-06-17 21:35:17 +0430

    Move files to /src/github.com in  in docker container

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM golang
+WORKDIR /go/src
 
 RUN go get gopkg.in/gorethink/gorethink.v4 && go install gopkg.in/gorethink/gorethink.v4
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,15 @@
 echo "\nWaiting for building and setting up the Tinode server...\nIt may take a few minutes.\n"
 
 # Install tinode server from mounted directory
-go get -tags rethinkdb tinode/chat/server
+go get -tags rethinkdb ./github.com/tinode/chat/server
 
 # Install tinode-db from mounted direcrory
-go get -tags rethinkdb tinode/chat/tinode-db
+go get -tags rethinkdb ./github.com/tinode/chat/tinode-db
 
 # Initiate database
-/go/bin/tinode-db -config=/go/src/tinode/chat/tinode-db/tinode.conf -data=/go/src/tinode/chat/tinode-db/data.json
+/go/bin/tinode-db -config=/go/src/github.com/tinode/chat/tinode-db/tinode.conf -data=/go/src/github.com/tinode/chat/tinode-db/data.json
 
 echo "\nThe Tinode server is starting...\n"
 
 # Run the server
-/go/bin/server -config=/go/src/tinode/chat/server/tinode.conf  -static_data=/web/example-react-js/
-
+/go/bin/server -config=/go/src/github.com/tinode/chat/server/tinode.conf  -static_data=/web/example-react-js/
